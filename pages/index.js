a9ccd2b32dee9f3b5782959f697575779d5aa2e8
ChristianTol <christian.tol1998@hotmail.com>
2022-10-20 12:54:01 +0200

    Explorer Nearby component added with api call.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import SmallCard from "../components/SmallCard";
+const https = require("https");
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -13,9 +18,36 @@ export default function Home() {
       <Header />
       <Banner />
 
-      <main>
-        
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* Pull some data from a server - API endpoints */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard 
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
+        </section>
       </main>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp", { agent }).
+  then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  }
 }
